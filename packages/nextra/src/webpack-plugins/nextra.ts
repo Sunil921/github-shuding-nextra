6d00737225ec0e8945c12a742eb582e3661b16ae
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-09-03 02:41:27 +0200

    ok seems fs.mkdir fixes error

--- packages/nextra/src/webpack-plugins/nextra.ts
+++ packages/nextra/src/webpack-plugins/nextra.ts
@@ -3,7 +3,6 @@ import { promises as fs } from 'graceful-fs'
 import type { Compiler } from 'webpack'
 import { CWD } from '../constants'
 import { PAGES_DIR } from '../file-system'
-import { pageMapCache } from '../page-map'
 import { collectFiles } from '../plugin'
 import type { Folder } from '../types'
 
@@ -12,13 +11,12 @@ export class NextraPlugin {
 
   apply(compiler: Compiler) {
     const pluginName = this.constructor.name
+    const { locales } = this.config
+    const chunksPath = join(CWD, '.next', 'static', 'chunks')
+
     compiler.hooks.beforeCompile.tapAsync(pluginName, async (_, callback) => {
-      const { locales } = this.config
       try {
         const result = await collectFiles({ dir: PAGES_DIR, locales })
-        pageMapCache.set(result)
-
-        const chunksPath = join(CWD, '.next', 'static', 'chunks')
 
         for (const locale of locales) {
           const folderItem =
@@ -28,7 +26,10 @@ export class NextraPlugin {
                   (item): item is Folder =>
                     'name' in item && item.name === locale
                 )
+
           if (!folderItem) continue
+
+          await fs.mkdir(join(chunksPath), { recursive: true })
 
           await fs.writeFile(
             join(chunksPath, `nextra-page-map-${locale}.json`),
@@ -42,7 +43,6 @@ export class NextraPlugin {
           `export const fileMap = ${JSON.stringify(result.fileMap, null, 2)}`,
           'utf8'
         )
-
         callback()
       } catch (error) {
         callback(error as Error)
